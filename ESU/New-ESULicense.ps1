4296f1b2724ab0e9b0acedc9019fbe90d0b352a4
juan <juan@obijuan.com>
2023-12-15 13:55:10 -0600

    Added functions and for loop

--- ESU/New-ESULicense.ps1
+++ ESU/New-ESULicense.ps1
@@ -1,41 +1,115 @@
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory=$true)]
+    [string]$licInfoFile
+)
+
 # Install the Azure PowerShell module if not already installed
-Install-Module -Name Az -Scope CurrentUser -Repository PSGallery -Force
+#Write-Host "Installing the Azure PowerShell module"
+#Install-Module -Name Az -Scope CurrentUser -Repository PSGallery -Force
 
 # Import the Azure PowerShell module
-Import-Module -Name Az
+#Write-Host "Importing the Azure PowerShell module"
+#Import-Module -Name Az
 
 # Connect to your Azure account
-Connect-AzAccount
+Write-Host "Connecting to your Azure account"
+Connect-AzAccount -ErrorAction Stop
 
 # Get an authentication token
 $token = (Get-AzAccessToken).Token
 
-# Set the subscription ID, resource group name, and license name
-$subscriptionId = "<your-subscription-id>"
-$resourceGroupName = "<your-resource-group-name>"
-$licenseName = "<your-license-name>"
 
-# Construct the API URL
-$url = "https://management.azure.com/subscriptions/$subscriptionId/resourceGroups/$resourceGroupName/providers/Microsoft.HybridCompute/licenses/$licenseName?api-version=2023-06-20-preview"
+function Get-LicenseInfo {
+    param(
+        [Parameter(Mandatory=$true)]
+        [object]$licInfo
+    )
+    # Read the license information from the file
+    $licenseName = $licInfo.licenseName
+    $type = $licInfo.type
+    $target = $licInfo.target
+    $edition = $licInfo.edition
+    $state = $licInfo.state
+    $processors = $licInfo.processors
+    $resourceGroupName = $licInfo.resourceGroupName
+    $subscriptionId = $licInfo.subscriptionId
+    $location = $licInfo.location
+    
+    Create-License -licenseName $licenseName -resourceGroupName $resourceGroupName -subscriptionId $subscriptionId -location $location `
+        -type $type -target $target -edition $edition -state $state -processors $processors
+}
 
-# Create a header with the authorization token
-$headers = @{'Authorization' = "Bearer $token"}
+function Create-License {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$licenseName,
+        [Parameter(Mandatory=$true)]
+        [string]$resourceGroupName,
+        [Parameter(Mandatory=$true)]
+        [string]$subscriptionId,
+        [Parameter(Mandatory=$true)]
+        [string]$location,
+        [Parameter(Mandatory=$true)]
+        [string]$type,
+        [Parameter(Mandatory=$true)]
+        [string]$target,
+        [Parameter(Mandatory=$true)]
+        [string]$edition,
+        [Parameter(Mandatory=$true)]
+        [string]$state,
+        [Parameter(Mandatory=$true)]
+        [int]$processors
+    )
+    # Construct the API URL
+    $url = "https://management.azure.com/subscriptions/$subscriptionId/resourceGroups/$resourceGroupName/providers/Microsoft.HybridCompute/licenses/" + $licenseName + "?api-version=2023-06-20-preview"
+    Write-Host "url: $url"
+    # Create a header with the authorization token
+    $headers = @{'Authorization' = "Bearer $token"}
 
-# Define the body as a JSON string
-$body = @"
+    # Define the body as a JSON string
+    $body = @"
     { 
-    "location": "ENTER-REGION",
+    "location": "$location",
         "properties": {
             "licenseDetails": {
-            "state": "Activated",
-            "target": "Windows Server 2012",
-            "Edition": "Standard",
-            "Type": "vCore",
-            "Processors": 12
+            "state": "$state",
+            "target": "$target",
+            "Edition": "$edition",
+            "Type": "$type",
+            "Processors": $processors
             }
         }
     }      
 "@
+    try {
+        # Invoke the REST API to create a license
+        Invoke-RestMethod -Method PUT -Uri $url -Headers $headers -Body $body -ContentType "application/json" -ErrorAction Stop
+    }
+    catch {
+        Write-Host -ForegroundColor Red "Error creating license $licenseName : $_"
+    }
 
-# Invoke the REST API to create a license
-Invoke-RestMethod -Method PUT -Uri $url -Headers $headers -Body $body
+}
+
+# Read the license information from the file
+Write-Host "Reading license information from $licInfoFile"
+try {
+    $AllLicInfo = Import-Csv -Path $licInfoFile -ErrorAction Stop
+    foreach ($licInfo in $AllLicInfo) {
+        Get-LicenseInfo -licInfo $licInfo
+    }
+}
+catch {
+    write-host -ForegroundColor Red "Error reading license information from $licInfoFile : $_"
+}
+Write-Host "Done"
+
+<# 
+    "licenseDetails": {
+    "state": "Activated",
+    "target": "Windows Server 2012",
+    "Edition": "Standard",
+    "Type": "vCore",
+    "Processors": 12 
+#>
